0edbd22e901492aaff471efdfc4577b3fea8eb2e
Taran Panesar <iamtaranpanesar@gmail.com>
2026-07-22 11:07:13 +0530

    Untrack all bin and obj folders globally across all projects

--- app.client/dev-dist/sw.js
+++ app.client/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.k10i78t2ns8"
+    "revision": "0.1h31osft778"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
